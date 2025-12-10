a9fbd752552c1ac9714fbc09494b1d840f976ece
emailhelper36-hue <emailhelper36@gmail.com>
2025-12-10 11:35:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,1 @@
-import streamlit as st, google.generativeai as genai
-from PIL import Image
-genai.configure(api_key="AIzaSyB5JAtiqSb1-ldEWoimdY8fgu7cjR3Yrgg")
-m = genai.GenerativeModel("gemini-2.5-flash")
-st.title("Plant Health Checker")
-f = st.file_uploader("Upload leaf image", type=["jpg","jpeg","png"])
-if f:
-    img = Image.open(f); st.image(img, caption="Uploaded leaf")
-    r = m.generate_content(["You are a plant doctor. Say if this leaf is healthy; if not, guess disease and give 3 simple fixes.", img])
-    st.write(r.text)
+
